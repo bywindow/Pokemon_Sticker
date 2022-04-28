a6026fa7e8b1934c9de8f5e0b2efc9300a362a8a
dionidip <paulus0617@gmail.com>
2022-04-29 01:52:40 +0900

    create loader view

---
+++ src/styles/LoaderStyled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const LoaderContainer = styled.div`
+  min-height: calc(100% - 380px);
+  width: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
+
+export default LoaderContainer;
---
+++ src/utils/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Loader from "react-loader-spinner"
+import LoaderContainer from '../styles/LoaderStyled';
+
+export default () => (
+  <LoaderContainer>
+    <Loader />
+  </LoaderContainer>
+);
