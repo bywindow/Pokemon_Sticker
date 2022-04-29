3cf9287af01a1dbd5311826724731b69aae01811
dionidip <paulus0617@gmail.com>
2022-04-29 17:20:56 +0900

    router

---
+++ src/utils/Router.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Redirect, Routes } from 'react-router-dom';
+
+//View
+
+
+function Routers (props) {
+
+  return (
+    <BrowserRouter>
+      {/* 헤더 영역 */}
+
+      <Routes>
+        {/* main view */}
+        {/* <Route path="/" element={} /> */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
