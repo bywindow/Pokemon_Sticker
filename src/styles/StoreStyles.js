d46182ac680d95c8986e70fac65edec75b6a0d2d
dionidip <paulus0617@gmail.com>
2022-05-01 23:35:19 +0900

    modal bug fix

--- src/styles/StoreStyles.js
+++ src/styles/StoreStyles.js
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 
 export const mainContainer = {
+  overlay: {
+    zIndex: 990,
+  },
+
   content: {
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
-    
+    zIndex: 991,
+    backgroundColor: 'black'
   }
 }
 
